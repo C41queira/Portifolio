f34efd342f9b8d726a31908ce0380748c8862d20
Caick <bertinviana@gmail.com>
2024-01-12 11:51:02 -0300

    Resposividade pagina de projetos e certificados (90%)

--- js/sliderScript.js
+++ js/sliderScript.js
@@ -1,17 +1,6 @@
 let count = 1 
 document.getElementById('radio-btn1').checked = true;
 
-var btnNext = document.getElementById('nextBTN')
-var btnPrevious = document.getElementById("previousBTN")
-
-btnNext.addEventListener("click", function(){
-    nextImg();
-})
-
-btnPrevious.addEventListener("click", function(){
-    previousImg();
-})
-
 function nextImg(){
     count++; 
     if (count > 3) {
@@ -20,42 +9,9 @@ function nextImg(){
     document.getElementById(`radio-btn${count}`).checked = true;
 }
 
-function previousImg(){
-    count--; 
-    if (count < 1) {
-        count = 1
-    }
-    document.getElementById(`radio-btn${count}`).checked = true;
-}
-
 // --------------------------------------------------------------------------------------------
 
-let c = 1 
-document.getElementById('radio-btn-project1').checked = true;
-
-var btnNextProject = document.getElementById('nextBTN-project')
-var btnPreviousProject = document.getElementById("previousBTN-project")
-
-btnNextProject.addEventListener("click", function(){
+setInterval(function () {
+    nextImg(); 
     nextImgProject();
-})
-
-btnPreviousProject.addEventListener("click", function(){
-    previousImgProject();
-})
-
-function nextImgProject(){
-    c++;    
-    if (c > 3) {
-        c = 1
-    }
-    document.getElementById(`radio-btn-project${c}`).checked = true;
-}
-
-function previousImgProject(){
-    c--; 
-    if (c < 1) {
-        c = 1
-    }
-    document.getElementById(`radio-btn-project${c}`).checked = true;
-}
+}, 6000)
